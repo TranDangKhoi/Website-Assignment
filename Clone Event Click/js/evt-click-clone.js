eb12631fb836c1e664108dac6571d6583333662c
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2021-11-20 21:54:06 +0700

    add inspect disability ( even though it is useless )

--- Clone Event Click/js/evt-click-clone.js
+++ Clone Event Click/js/evt-click-clone.js
@@ -49,6 +49,29 @@ function checkReq(){
     }
 }
 
+document.addEventListener('contextmenu', event => event.preventDefault());
+
 var username = document.getElementById('username');
 
+
+/* check inspect disablity even though i know it meaningless */
+
 username.addEventListener('blur',checkEmail,false);
+
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+       return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+       return false;
+    }
+  }
