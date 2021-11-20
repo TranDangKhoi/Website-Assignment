e612063c21a1b4c96bfba9fd8f2caf3bd0bf66f9
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2021-11-20 23:19:18 +0700

    fix some bugs and add fb for password too

--- Clone Event Click/js/evt-click-clone.js
+++ Clone Event Click/js/evt-click-clone.js
@@ -19,13 +19,13 @@ elClose.addEventListener('click',dismissNote,false); // remove pop-up khi click 
 /* For username */
 
 document.getElementById('submit').onmouseover = function(){
-    if (username.value.length >= 7){
+    if (username.value.length >= 7 && password.value.length >= 8){
         document.getElementById('submit').style.backgroundColor = "rgb(167, 69, 69)";
     }
 }
 
 document.getElementById('submit').onmouseleave = function(){
-    if (username.value.length >= 7){
+    if (username.value.length >= 7 && password.value.length >= 8){
         document.getElementById('submit').style.backgroundColor = "rgb(211, 96, 96)";
     }
 }
@@ -35,17 +35,10 @@ function checkEmail(){
     elMsg = document.getElementById("feedback");
     if (username.value.length >= 7){
         elMsg.textContent = "";
-        document.getElementById('submit').style.backgroundColor = "rgb(211, 96, 96)";
     }
     else{
-        elMsg.textContent = "Your username should be more than 7 characters long";
-        document.getElementById('submit').style.backgroundColor = "rgb(160, 138, 138)";
-    }
-}
-
-function checkReq(){
-    if (username.value.length >= 7){
-        window.location.href = "https://github.com/TranDangKhoi";
+        elMsg.textContent = "Your username must be more than 7 characters long";
+        
     }
 }
 
@@ -53,10 +46,42 @@ document.addEventListener('contextmenu', event => event.preventDefault());
 
 var username = document.getElementById('username');
 
+/* For Password */
 
-/* check inspect disablity even though i know it meaningless */
+function checkPassword(){
+        elMsg = document.getElementById("feedback2");
+    if (password.value.length >= 8){
+        elMsg.textContent = "";
+    }
+    else{
+        elMsg.textContent = "Your password must be more than 8 characters long"
+    }
+}
+
+var signin = document.getElementById('submit');
 
 username.addEventListener('blur',checkEmail,false);
+password.addEventListener('blur',checkPassword,false);
+username.addEventListener('blur',checkBoth,false);
+password.addEventListener('blur',checkBoth,false);
+/* Change sign in background button when both of username and password met the requirements */
+
+function checkBoth(){
+    if(username.value.length >= 7 && password.value.length >= 8 ){
+        document.getElementById('submit').style.backgroundColor = "rgb(211, 96, 96)";
+    }
+    else {
+        document.getElementById('submit').style.backgroundColor = "rgb(160, 138, 138)";
+    }
+}
+
+/* check xem da du dieu kien chua moi cho chuyen trang */
+function checkReq(){
+    if (username.value.length >= 7 && password.value.length >= 8 ){
+        window.location.href = "https://github.com/TranDangKhoi";
+    }
+}
+/* check inspect disablity even though i know it meaningless */
 
 document.onkeydown = function(e) {
     if(event.keyCode == 123) {
